83324c016fe164c5018428bae6d107f93e3d98c5
Oksana <oksana@Oksanas-MacBook-Air.local>
2020-07-16 13:02:51 +0300

    first commit

---
+++ clean_text.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import re
+import unicodedata
+import html2text
+from sqlalchemy import create_engine
+
+chunksize = 1000
+user = 'postgres'
+password = 'secret'
+db_name = 'media_ecosystem'
+table_name = 'news_items'
+
+def read_table():
+    db_url = 'postgresql://localhost/{}?user={}&password={}'.format(db_name, user, password)
+    sql_engine = create_engine(db_url, echo=False)
+    conn = sql_engine.connect()
+
+    return pd.read_sql_table(table_name, conn, chunksize=chunksize)  
+
+
+
+def get_text_from_html(output_file):
+    # HTML2Text keeps formatting, so all paragraphes are saved
+    # you get markdown text, so headers are marked with '#' and lists with '*'
+    text_maker = html2text.HTML2Text()
+    # Do not include any formatting for links
+    text_maker.ignore_links = True
+    # Do not include any formatting for images
+    text_maker.ignore_images = True
+    # for using unicode
+    text_maker.unicode_snob = True
+    text_maker.body_width = 0
+    text_maker.single_line_break = True
+    # Ignore all emphasis formatting in the html.
+    text_maker.ignore_emphasis = True
+
+    table_chunks = read_table()
+
+    df_parts = []
+
+    for chunk in table_chunks:
+        chunk['text'] = chunk.text.apply(lambda x: text_maker.handle(x) if pd.notnull(x) else None)
+        df_parts.append(chunk)
+        del chunk
+    df = pd.concat(df_parts)
+    df.to_csv(output_file, index=False)
+
+
+# based on https://github.com/brown-uk/nlp_uk/blob/master/src/main/groovy/org/nlp_uk/other/CleanText.groovy
+
+lat_to_cyr_map = {
+    'a': 'а',
+    'c': 'с',
+    'e': 'е',
+    'i': 'і',
+    'l': 'І',
+    'o': 'о',
+    'u': 'и',
+    'p': 'р',
+    'n': 'п',
+    'm': 'т',
+    'x': 'х',
+    'y': 'у',
+    'k': 'к',
+    'b': 'ь',
+    'r': 'г',
+    'A': 'А',
+    'B': 'В',
+    'C': 'С',
+    'E': 'Е',
+    'H': 'Н',
+    'I': 'І',
+    'K': 'К',
+    'M': 'М',
+    'O': 'О',
+    'P': 'Р',
+    'T': 'Т',
+    'X': 'Х',
+    'Y': 'У',
+    "á": "а́",
+    "Á": "А́",
+    "é": "е́",
+    "É": "Е́",
+    "í": "і́",
+    "Í": "І́",
+    "ḯ": "ї́",
+    "Ḯ": "Ї́",
+    "ó": "о́",
+    "Ó": "О́",
+    "ú": "и́",
+    "ý": "у́",
+    "Ý": "У́",
+    "0": "о"
+}
+
+cyr_to_lat_map = {}
+for k, v in lat_to_cyr_map.items():
+    cyr_to_lat_map[v] = k
+
+APOSTROPHY_LIKE = ('”',
+                   '‟',
+                   '"',
+                   '‘',
+                   '′',
+                   '\u0313',
+                   '΄',
+                   '’',
+                   '´',
+                   '`',
+                   '’',
+                   '?',
+                   '*',
+                   )
+APOSTROPHY_PREFIX = 'бвгґдзкмнпрстфхш'
+APOSTROPHY_SUFFIX = 'єїюя'
+
+
+def clean(text):
+    if pd.notnull(text):
+        text = unicodedata.normalize("NFKC", text)
+        text = re.sub(r"\\n", "\n", text)
+        text = re.sub(r"\n{2,}", "\n", text)
+        text = re.sub(r" {2,}", " ", text)
+        text = text.strip()
+
+        # change strange apostrophe to '
+        text = re.sub(r"([{prefix}])[{apostrophy}]([{suffix}])".format(
+            prefix=APOSTROPHY_PREFIX, apostrophy=''.join(APOSTROPHY_LIKE), suffix=APOSTROPHY_SUFFIX),
+            r"\1'\2", text, flags=re.IGNORECASE)
+        text = re.sub(r"([{prefix}])&#39\s?([{suffix}])".format(
+            prefix=APOSTROPHY_PREFIX, suffix=APOSTROPHY_SUFFIX), r"\1'\2", text)
+
+        # add space between sentences if needed
+        text = re.sub(r"([\.\?\!])([А-ЯІЇЄҐA-Z])", r"\1 \2", text)
+       
+        # clean up latin/cyrillic character mix
+        # cases:
+        # - latin symbols that look like cyrillic in ukrainian words
+        # - cyrillic symbols that look like latin in english words
+        text = re.sub(
+            r"([бвгґдєжзийклмнптфцчшщьюяБГҐДЄЖЗИЙЛПФХЦЧШЩЬЮЯ]['’ʼ]?)([aceiopxyunmkbr0ABCEHIKMOPTXYáÁéÉíÍḯḮóÓúýÝ])",
+            lambda x: x.group(1) + lat_to_cyr_map[x.group(2)], text)
+
+        text = re.sub(
+            r"([aceiopxyaceiopxyunmkbr0ABCEHIKMOPTXYáÁéÉíÍḯḮóÓúýÝ])(['’ʼ]?[бвгґдєжзийклмнптфцчшщьюяБГҐДЄЖЗИЙЛПФХЦЧШЩЬЮЯ])",
+            lambda x: lat_to_cyr_map[x.group(1)] + x.group(2), text)
+
+        text = re.sub(r"([bdfghjklmnrstuvwzDFGJLNQRSUVWZ]['’ʼ]?)([асеіорхуАВСЕНІКМНОРТХУ])",
+                      lambda x: x.group(1) + cyr_to_lat_map[x.group(2)], text)
+
+        text = re.sub(r"([асеіорхуАВСЕНІКМНОРТХУ])(['’ʼ]?[bdfghjklmnrstuvwzDFGJLNQRSUVWZ])",
+                      lambda x: cyr_to_lat_map[x.group(1)] + x.group(2), text)
+
+        text = re.sub(r"([а-яіїєґА-ЯІЇЄҐ]['’ʼ]?)([aceiopxyunmkbr0ABCEHIKMHOPTXYáÁéÉíÍḯḮóÓúýÝ])(['’ʼ]?[а-яіїєґА-ЯІЇЄҐ])",
+                      lambda x: x.group(1) + lat_to_cyr_map[x.group(2)] + x.group(3), text)
+
+        text = re.sub(r"([a-zA-Z]['’ʼ]?)([асеіорхуАВСЕНІКМНОРТХУ])(['’ʼ]?[a-zA-Z])",
+                      lambda x: x.group(1) + cyr_to_lat_map[x.group(2)] + x.group(3), text)
+
+        text = re.sub(r"([а-яіїєґ]\W{0,2} )([ayico])( [А-ЯІЇЄҐа-яіїєґ])",
+                      lambda x: x.group(1) + lat_to_cyr_map[x.group(2)] + x.group(3), text)
+
+        text = re.sub(r"([AIYBKOl])( [А-ЯІЇЄҐа-яіїєґ])",
+                      lambda x: lat_to_cyr_map[x.group(1)] + x.group(2), text)
+
+    return text
+
+
+def text_cleaning(df):
+    df['text'] = df.text.apply(clean)
+    df['title'] = df.title.apply(clean)
+    df['subtitle'] = df.subtitle.apply(clean)
+    return df
+
+   
---
+++ strip_text.py
@@ -0,0 +1,149 @@
+import re
+import pandas
+
+strip_patterns = {
+    'all': [
+        (r"\*\[25 лютого\]: за юліанським календарем.*", re.S),
+        (r"^[\w ]*фото:.*?\n", re.M | re.I),
+        (r"Теги:.*", re.S),
+        (r"Читайте також: ?\n?\w+.*\n?", re.M),
+        (r"Якщо побачили помилку.*", re.S), 
+        (r"(^> \w+.*\n)+", re.M), 
+        (r"этот материал доступен на русском", re.I),
+        (r"читать (новость )?на русском", re.I),
+        (r"Цей матеріал можна прочитати і російською мовою\n", re.I),
+        (r"^Реклама\n", re.M),
+        (r"^Твитнуть\n", re.M)
+    ],
+    'apostrophe.ua': [
+        (r"### Новини.*?Всі новини", re.S),
+        (r"Версія для друку.*", re.S)
+    ],
+    'https://www.rbc.ua': [
+        (r"Читайте нас в Google News та підписуйтесь.*", re.S),
+        (r"Надіслати новину другу.*", re.S)
+    ],
+    'https://www.unian.ua': [
+        (r"Читайте останні новини України та світу.*", re.S),
+        (r"Якщо ви знайшли помилку.*", re.S),
+        (r"^Читайте також.*?\n", re.M)
+    ],
+    'https://censor.net.ua': [
+        (r"^.*?Редактор Цензор\. НЕТ", re.S),
+        (r"Мне нравится.*", re.S),
+        (r"^Читайте (также )?на \"Цензор\. НЕТ\":.*?\n", re.M),
+        (r"^Все про:.*?\n", re.M),
+        (r"Топ комментарии.*", re.S)
+    ],
+    'https://politeka.net/uk': [
+        (r"Популярні новини зараз.*?Показати ще", re.S)
+    ],
+    'www.radiosvoboda.org': [
+        (r"^ДИВІТЬСЯ ТАКОЖ: ?\n####.*?\n", re.M),
+        (r"### FACEBOOК КОМЕНТАРІ.*", re.S),
+        (r"Отримати Adobe Flash Player.*\* \d+p | \d+(\,?\d+)?MB", re.S)
+    ],
+    'https://dt.ua/': [
+        (r"(\^* #[\w ]+)+", re.M)
+    ],
+    'https://hromadske.ua/': [
+        (r"Поділитись.*", re.S),
+        (r"Більше про:.*", re.S),
+        (r"^Інформаційна гігієна не менш важлива за особисту.*?\n", re.M),
+        (r"### читайте також ?\n.*\n.*\n", re.M) ###???
+    ],
+    'https://www.obozrevatel.com': [
+        (r"Тисни! Підписуйся!.*", re.S),
+        (r"Ти ще не підписаний на наш Telegram.*", re.S),
+        (r"Не набридаємо! Тільки найважливіше.*", re.S),
+        (r"Підписуйся на наш Telegram.*", re.S),
+        (r"^Читайте:.*?\n", re.M | re.I)
+    ],
+    'https://www.segodnya.ua/ua': [
+        (r"^Також дивіться.*?\n", re.M),
+        (r"Підпишись на наш telegram.*", re.S),
+        (r"##### Ще кілька матеріалів по темі:.*", re.S)
+    ],
+    'focus.ua': [
+        (r"^Читайте также.*\n", re.M),
+#         (r"Как ранее сообщал Фокус:.*", re.S)
+    ],
+    '112.ua':[
+        (r"^#\w+.*?#\w+.*\n", re.M),
+        (r"^Новини за темою:.*?\n", re.M)
+    ],
+    'https://www.liga.net': [
+        (r"^Читайте нас в Telegram:.*?\n", re.M),
+        (r"Еще по теме.*", re.S),
+        (r"Ще по темі.*", re.S),
+        (r"^Читайте также.*\n", re.M)
+    ],
+    'espreso.tv':[
+        (r"Слідкуйте за подіями в Україні та світі.*", re.S),
+        (r"Все по темі.*", re.S),
+        (r"Підписуйтесь на Telegram.*", re.S)
+        # can't detect this parts in all domain, so leaving them for now
+#         (r" \* Нагадаємо")
+    ],
+    'strana.ua': [
+        (r"^Читайте также ?\n( ?\*.*?\n)+", re.M),
+        (r"Подпишитесь на телеграм-канал.*", re.S),
+        (r"Читайте Страну в Google News.*", re.S)
+    ],
+    'tsn.ua': [
+        (r"^\* Facebook.*\* Відправити лист", re.S),
+#         (r"###### Читайте також: ?\n\w+.*?\n", False),
+        (r"Приєднуйтесь також до tsn\.ua у Google News.*", re.S),
+         (r"^ ?\* ?(Facebook|Twitter|Telegram|Messenger|Viber)\n", re.M)
+    ],
+    'https://hromadske.radio': [
+        (r"Підписуйтесь на Telegram-канал.*", re.S)
+    ],
+    'znaj.ua': [
+        (r"Обов'язково підпишись.*", re.S),
+        (r"Популярні новини зараз.*Показати ще", re.S),
+        (r"Популярні новини.*", re.S)
+    ],
+    'fakty.com.ua': [
+        (r"^Читайте:.*?\n", re.M)
+    ],
+    'https://www.epravda.com.ua': [
+        (r"Читайте детальніше:.*", re.S)
+#         (r"Нагадуємо:.*", re.S)
+    ],
+    'https://ukr.lb.ua': [
+        (r"Читайте головні новини LB\.ua.*", re.S),
+        (r"^(Facebook|Twitter)\n", re.M),
+        (r"^Темы:.*", re.S|re.M)
+    ],
+    'https://ukranews.com': [
+        (r"Больше новостей о:.*", re.S),
+        (r"Коментарии Facebook.*", re.S)
+    ],
+    'www.unn.com.ua' : [],
+    
+    'https://ua.interfax.com.ua': [],
+    
+    '24tv.ua': [],
+    
+    'gordonua.com': [],
+    
+    'zik.ua': [
+        (r"^Новини за темою:.*?\n", re.M)
+    ],
+    'https://www.pravda.com.ua': [
+#         (r"Нагадаємо:.*", re.S),
+    ]
+    
+}
+
+
+def mystip(df):
+    for part_to_strip in strip_patterns['all']:
+        df.text.update(df.text.str.replace(part_to_strip[0], "", flags=part_to_strip[1]))
+
+    for d in df.domain.unique():
+        for part_to_strip in strip_patterns[d]:
+            df.text.update(df[domain==d].text.str.replace(part_to_strip[0], "", flags=part_to_strip[1]))
+
+
